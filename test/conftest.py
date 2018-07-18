ba08fede093a5281b29e3931995fb2f89941ec6b
Bogdan Opanchuk <bogdan@opanchuk.net>
2018-07-18 21:45:42 +1000

    test/conftest: add options for selecting APIs/devices to run tests on

--- test/conftest.py
+++ test/conftest.py
@@ -1,20 +1,59 @@
 import pytest
 
-from reikna.cluda import cuda_api, ocl_api
+from reikna.cluda import cuda_api, ocl_api, get_api, supported_api_ids, find_devices
 
 from tfhe.computation_cache import clear_computation_cache
 
 
-@pytest.fixture(scope='session', params=["cuda"])
+def pytest_addoption(parser):
+    api_ids = supported_api_ids()
+
+    parser.addoption("--api", action="store",
+        help="Backend API. If 'supported' is chosen, will run on all available ones.",
+        default="supported", choices=api_ids + ["supported"])
+    parser.addoption("--device-include-mask", action="append",
+        help="Run tests on matching devices only",
+        default=[])
+    parser.addoption("--include-duplicate-devices", action="store_true",
+        help="Run tests on all available devices and not only on uniquely named ones",
+        default=False)
+
+
+def pytest_generate_tests(metafunc):
+
+    config = metafunc.config
+
+    if 'thread' in metafunc.fixturenames:
+
+        api_opt = config.option.api
+        api_ids = supported_api_ids() if api_opt == 'supported' else [api_opt]
+
+        vals = []
+        ids = []
+        for api_id in api_ids:
+            api = get_api(api_id)
+            devices = find_devices(
+                api,
+                include_devices=config.option.device_include_mask,
+                include_duplicate_devices=config.option.include_duplicate_devices)
+            for pnum in sorted(devices.keys()):
+                dnums = sorted(devices[pnum])
+                for dnum in dnums:
+                    vals.append((api_id, pnum, dnum))
+                    ids.append("{api_id}:{pnum}:{dnum}".format(api_id=api_id, pnum=pnum, dnum=dnum))
+
+        metafunc.parametrize("thread", vals, ids=ids, indirect=True)
+
+
+@pytest.fixture(scope='session')
 def thread(request):
-    if request.param == 'cuda':
-        api = cuda_api()
-        platform = api.get_platforms()[0]
-        device = platform.get_devices()[0]
-    else:
-        api = ocl_api()
-        platform = api.get_platforms()[0]
-        device = platform.get_devices()[2]
+
+    api_id, pnum, dnum = request.param
+
+    api = get_api(api_id)
+
+    platform = api.get_platforms()[pnum]
+    device = platform.get_devices()[dnum]
 
     thread = api.Thread(device)
     yield thread
