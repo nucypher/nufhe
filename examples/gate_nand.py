8cbdc8309ae5079d6150a63339af2dbe122d6fa9
Bogdan Opanchuk <bogdan@opanchuk.net>
2018-09-26 11:03:10 +1000

    examples/gate_nand: switch to using secret/cloud key terminology

--- examples/gate_nand.py
+++ examples/gate_nand.py
@@ -5,20 +5,20 @@ from reikna.cluda import any_api
 thr = any_api().Thread.create(interactive=True)
 
 rng = numpy.random.RandomState()
-private_key, public_key = nufhe.make_key_pair(thr, rng)
+secret_key, cloud_key = nufhe.make_key_pair(thr, rng)
 
 size = 32
 
 bits1 = rng.randint(0, 2, size=size).astype(numpy.bool)
 bits2 = rng.randint(0, 2, size=size).astype(numpy.bool)
 
-ciphertext1 = nufhe.encrypt(thr, rng, private_key, bits1)
-ciphertext2 = nufhe.encrypt(thr, rng, private_key, bits2)
+ciphertext1 = nufhe.encrypt(thr, rng, secret_key, bits1)
+ciphertext2 = nufhe.encrypt(thr, rng, secret_key, bits2)
 
 reference = ~(bits1 * bits2)
 
-result = nufhe.empty_ciphertext(thr, public_key.params, ciphertext1.shape)
-nufhe.gate_nand(thr, public_key, result, ciphertext1, ciphertext2)
+result = nufhe.empty_ciphertext(thr, cloud_key.params, ciphertext1.shape)
+nufhe.gate_nand(thr, cloud_key, result, ciphertext1, ciphertext2)
 
-result_bits = nufhe.decrypt(thr, private_key, result)
+result_bits = nufhe.decrypt(thr, secret_key, result)
 assert (result_bits == reference).all()
